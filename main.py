ababb1bb4f0f6dd308ed818a0789ddc15f001388
motyliak <67186212+motyliak@users.noreply.github.com>
2020-06-20 14:06:52 +0200

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,79 @@
+def on_button_pressed_a():
+    global ze1, zl1, ce1, ze2, zl2, ce2, chyba
+    if ze1 == 1:
+        ze1 = 0
+        zl1 = 1
+        ce1 = 0
+        ze2 = 0
+        zl2 = 1
+        ce2 = 1
+    elif ce1 == 0 and zl1 == 1:
+        ze1 = 0
+        zl1 = 0
+        ce1 = 1
+        ze2 = 1
+        zl2 = 0
+        ce2 = 0
+    elif zl1 == 0 and ce1 == 1:
+        ce1 = 1
+        zl1 = 1
+        ze1 = 0
+        ze2 = 0
+        zl2 = 1
+        ce2 = 0
+    elif zl1 == 1 and ce1 == 1:
+        ce1 = 0
+        zl1 = 0
+        ze1 = 1
+        ze2 = 0
+        zl2 = 0
+        ce2 = 1
+    if chyba == 1:
+        ce1 = 1
+        zl1 = 0
+        ze1 = 0
+        ze2 = 1
+        zl2 = 0
+        ce2 = 0
+        chyba = 0
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
+    global chyba
+    chyba = 1
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+zl2 = 0
+ze2 = 0
+chyba = 0
+ce2 = 0
+ce1 = 0
+zl1 = 0
+ze1 = 0
+ze1 = 1
+zl1 = 0
+ce1 = 0
+ce2 = 1
+chyba = 0
+led.enable(False)
+
+def on_forever():
+    if chyba == 0:
+        pins.digital_write_pin(DigitalPin.P0, ze1)
+        pins.digital_write_pin(DigitalPin.P1, zl1)
+        pins.digital_write_pin(DigitalPin.P2, ce1)
+        pins.digital_write_pin(DigitalPin.P3, ze2)
+        pins.digital_write_pin(DigitalPin.P4, zl2)
+        pins.digital_write_pin(DigitalPin.P5, ce2)
+    else:
+        pins.digital_write_pin(DigitalPin.P0, 0)
+        pins.digital_write_pin(DigitalPin.P1, 0)
+        pins.digital_write_pin(DigitalPin.P2, 0)
+        pins.digital_write_pin(DigitalPin.P3, 0)
+        pins.digital_write_pin(DigitalPin.P4, 0)
+        pins.digital_write_pin(DigitalPin.P5, 0)
+        basic.pause(200)
+        pins.digital_write_pin(DigitalPin.P1, 1)
+        pins.digital_write_pin(DigitalPin.P4, 1)
+        basic.pause(200)
+basic.forever(on_forever)
---
+++ main.ts
@@ -0,0 +1,77 @@
+input.onButtonPressed(Button.A, function () {
+    if (ze1 == 1) {
+        ze1 = 0
+        zl1 = 1
+        ce1 = 0
+        ze2 = 0
+        zl2 = 1
+        ce2 = 1
+    } else if (ce1 == 0 && zl1 == 1) {
+        ze1 = 0
+        zl1 = 0
+        ce1 = 1
+        ze2 = 1
+        zl2 = 0
+        ce2 = 0
+    } else if (zl1 == 0 && ce1 == 1) {
+        ce1 = 1
+        zl1 = 1
+        ze1 = 0
+        ze2 = 0
+        zl2 = 1
+        ce2 = 0
+    } else if (zl1 == 1 && ce1 == 1) {
+        ce1 = 0
+        zl1 = 0
+        ze1 = 1
+        ze2 = 0
+        zl2 = 0
+        ce2 = 1
+    }
+    if (chyba == 1) {
+        ce1 = 1
+        zl1 = 0
+        ze1 = 0
+        ze2 = 1
+        zl2 = 0
+        ce2 = 0
+        chyba = 0
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    chyba = 1
+})
+let zl2 = 0
+let ze2 = 0
+let chyba = 0
+let ce2 = 0
+let ce1 = 0
+let zl1 = 0
+let ze1 = 0
+ze1 = 1
+zl1 = 0
+ce1 = 0
+ce2 = 1
+chyba = 0
+led.enable(false)
+basic.forever(function () {
+    if (chyba == 0) {
+        pins.digitalWritePin(DigitalPin.P0, ze1)
+        pins.digitalWritePin(DigitalPin.P1, zl1)
+        pins.digitalWritePin(DigitalPin.P2, ce1)
+        pins.digitalWritePin(DigitalPin.P3, ze2)
+        pins.digitalWritePin(DigitalPin.P4, zl2)
+        pins.digitalWritePin(DigitalPin.P6, ce2)
+    } else {
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        pins.digitalWritePin(DigitalPin.P2, 0)
+        pins.digitalWritePin(DigitalPin.P3, 0)
+        pins.digitalWritePin(DigitalPin.P4, 0)
+        pins.digitalWritePin(DigitalPin.P6, 0)
+        basic.pause(200)
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        pins.digitalWritePin(DigitalPin.P4, 1)
+        basic.pause(200)
+    }
+})
